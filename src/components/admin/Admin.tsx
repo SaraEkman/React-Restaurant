674d38643a153ea1203c48aa5a63cc1d4fbd560e
johanna-almqvist <94167705+johanna-almqvist@users.noreply.github.com>
2022-04-14 12:05:54 +0200

    Fixat avbokningar

--- src/components/admin/Admin.tsx
+++ src/components/admin/Admin.tsx
@@ -1,16 +1,52 @@
 import { useEffect, useState } from "react";
+import { Container } from "react-bootstrap/";
 import { IBooking } from "../../models/IBooking";
 import { GetAdminService } from "../../services/GetAdminService";
 
 export function Admin() {
   const [bookings, setBookings] = useState<IBooking[]>([]);
 
-  useEffect(() => {
-    let service = new GetAdminService();
-    service
-      .getBookings("624c2f5347678330c7a5c58e")
-      .then((bookings) => setBookings(bookings));
-  }, []);
+  const service = new GetAdminService();
+  let getBookings = service.getBookings;
 
-  return <p>Dessa bokningar finns: {bookings.length}</p>;
+  useEffect(() => {
+    getBookings("624c2f5347678330c7a5c58e").then((bookings) =>
+      setBookings(bookings)
+    );
+  }, [getBookings]);
+
+  function deleteBooking(id: string) {
+    service.deleteBooking(id).then(() => {
+      const filteredBookings = bookings.filter((booking) => {
+        return id !== booking._id;
+      });
+      setBookings(filteredBookings);
+    });
+  }
+
+  let ShowBokings = bookings.map((booking, i) => {
+    return (
+      <div key={booking._id}>
+        <p>id{booking._id}</p>
+        <p>Datum: {booking.date}</p>
+        <p>Antal personer: {booking.numberOfGuests}</p>
+        <p>Tid: {booking.time}</p>
+        <p>Customer Id {booking.customerId}</p>
+        <button
+          onClick={() => {
+            deleteBooking(booking._id);
+          }}
+        >
+          Avboka
+        </button>
+      </div>
+    );
+  });
+
+  return (
+    <>
+      <h3>Alla Bokningar :</h3>
+      <Container>{ShowBokings}</Container>
+    </>
+  );
 }
--- src/services/GetAdminService.ts
+++ src/services/GetAdminService.ts
@@ -4,7 +4,13 @@ import { IBooking } from "../models/IBooking";
 export class GetAdminService {
   async getBookings(id: string): Promise<IBooking[]> {
     let response = await axios.get<IBooking[]>(
-      `https://school-restaurant-api.azurewebsites.net/booking/${id}`
+      `https://school-restaurant-api.azurewebsites.net/booking/restaurant/${id}`
+    );
+    return response.data;
+  }
+  async deleteBooking(id: string): Promise<any> {
+    let response = await axios.delete(
+      `https://school-restaurant-api.azurewebsites.net/booking/delete/${id}`
     );
     return response.data;
   }
